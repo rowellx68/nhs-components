2ef19a76d6ac430bc9ef018fb52f21448f093422
Rowell Heria <rowell+dev@heria.uk>
2023-07-20 19:06:23 +0100

    test: update fieldset test

--- packages/nhs-components-react/src/components/form-elements/fieldset/__tests__/Fieldset.test.tsx
+++ packages/nhs-components-react/src/components/form-elements/fieldset/__tests__/Fieldset.test.tsx
@@ -2,10 +2,31 @@ import { render, screen } from '@/utils/test-utils'
 import { describe, test, expect } from 'vitest'
 import Fieldset from '../Fieldset'
 import { Size } from '@/types/nhsuk-sizes'
+import Input from '../../input'
 
 describe('Fieldset', () => {
   test('should match the snapshot', () => {
     render(<Fieldset data-testid="fieldset" />)
+
+    expect(screen.getByTestId('fieldset')).toMatchSnapshot()
+  })
+
+  test('should match the snapshot - registered elements', () => {
+    render(
+      <Fieldset data-testid="fieldset">
+        <Input id="sample-input" />
+      </Fieldset>,
+    )
+
+    expect(screen.getByTestId('fieldset')).toMatchSnapshot()
+  })
+
+  test('should match the snapshot - errored elements', () => {
+    render(
+      <Fieldset data-testid="fieldset">
+        <Input id="sample-input" error="this is an error" />
+      </Fieldset>,
+    )
 
     expect(screen.getByTestId('fieldset')).toMatchSnapshot()
   })
