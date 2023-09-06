4f02faa1084decfa452fc35f34a6763de87f88a1
Rowell Heria <rowell+dev@heria.uk>
2023-09-06 20:49:42 +0100

    docs: add jsdoc for expander and summarylist

--- packages/nhs-components-react/src/components/content-presentation/expander/Expander.tsx
+++ packages/nhs-components-react/src/components/content-presentation/expander/Expander.tsx
@@ -7,6 +7,21 @@ type Expander = Omit<Details, 'ExpanderGroup' | 'expander'> &
 const Summary = Details.Summary
 const Text = Details.Text
 
+/**
+ * Make a complex topic easier to digest by letting users reveal more detailed information only if they need it.
+ *
+ * For more information on when to use this component, got to the [NHS Service Manuals website](https://service-manual.nhs.uk/design-system/components/expander).
+ *
+ * @link https://service-manual.nhs.uk/design-system/components/expander
+ *
+ * @example
+ *
+ * ```tsx
+ * <Expander>
+ *  <Expander.Summary>How to find your NHS number</Expander.Summary>
+ *  <Expander.Text>You can find your NHS number...</Expander.Text>
+ * </Expander>
+ */
 const Expander: Expander = ({ children, className, ...rest }): JSX.Element => {
   return (
     <Details expander className={className} {...rest}>
--- packages/nhs-components-react/src/components/content-presentation/summary-list/SummaryList.tsx
+++ packages/nhs-components-react/src/components/content-presentation/summary-list/SummaryList.tsx
@@ -60,6 +60,27 @@ const Actions: React.FC<HTMLProps<HTMLDivElement>> = ({
   )
 }
 
+/**
+ * Use the summary list to summarise information, for example, a user’s responses at the end of a form.
+ *
+ * For more information on when to use this component, go to the [NHS Service Manual website](https://service-manual.nhs.uk/design-system/components/summary-list).
+ *
+ * @link https://service-manual.nhs.uk/design-system/components/summary-list
+ *
+ * @example
+ *
+ * ```tsx
+ * <SummaryList>
+ *  <SummaryList.Row>
+ *   <SummaryList.Key>Name</SummaryList.Key>
+ *   <SummaryList.Value>Dr John Smith</SummaryList.Value>
+ *   <SummaryList.Actions>
+ *    <a href="#">Change<span className="nhsuk-u-visually-hidden"> name</span></a>
+ *   </SummaryList.Actions>
+ *  </SummaryList.Row>
+ * </SummaryList>
+ * ```
+ */
 const SummaryList: SummaryList = ({
   children,
   className,
