98e0395dcce74b352b5675236834295376771c38
Rowell Heria <rowell+dev@heria.uk>
2023-11-30 18:44:05 +0000

    feat: add character count component

--- packages/nhsuk-frontend-react/src/experimental.ts
+++ packages/nhsuk-frontend-react/src/experimental.ts
@@ -1,2 +1,5 @@
 // content-presentation
 export { default as Tabs } from '@/experimental/components/content-presentation/tabs'
+
+// form-elements
+export { default as CharacterCount } from '@/experimental/components/form-elements/character-count'
---
+++ packages/nhsuk-frontend-react/src/lib/experimental/components/form-elements/character-count/index.ts
@@ -0,0 +1,3 @@
+import CharacterCount from './CharacterCount'
+
+export default CharacterCount
