ca8332fe62cc43f8604ba7ff884c93a09bde618a
Rowell Heria <rowell+dev@heria.uk>
2024-05-27 23:01:53 +0100

    Deploying to gh-pages from @ rowellx68/nhs-components@5fec2b6c1771117d18fe19786366100bec793181

---
+++ assets/Checkbox.Divider.stories-Bpqthh-G.js
@@ -0,0 +1,20 @@
+import{C as t,W as a,m as i}from"./Checkbox.stories-nk49cWBu.js";import{R as e}from"./index-uCp2LrAq.js";import"./FormGroup-BvpLCAgb.js";import"./Hint-Cy41Y5xV.js";import"./clsx-B-dksMZM.js";import"./Label-BVxFGFqs.js";import"./Base-CQ_ipzy_.js";import"./ErrorMessage-C5IFgkMq.js";import"./VisuallyHidden-SShDg0Sg.js";import"./polymorphic-factory-C1jPlyAy.js";import"./use-id-with-prefix-ctfEntwL.js";import"./factory-Dl037jXY.js";import"./common-DS4CpZRv.js";import"./Input-DKuhsIBb.js";import"./_commonjsHelpers-BosuxZz1.js";const f={title:"Components/Form Elements/Checkbox/Checkbox.Divider",component:t.Divider,argTypes:{children:{control:!1,table:{type:{summary:"ReactNode"}}}},decorators:[r=>e.createElement("form",null,e.createElement(r,null))]},o={args:{children:"or"},render:r=>e.createElement(t,{...Object.assign({},a.args,i.args)},e.createElement(t.Item,{value:"sore throat",exclusiveGroup:"symptoms-list"},"Sore throat"),e.createElement(t.Item,{value:"runny nose",exclusiveGroup:"symptoms-list"},"Runny nose"),e.createElement(t.Item,{value:"muscle or joint pain",exclusiveGroup:"symptoms-list"},"Muscle or joint pain"),e.createElement(t.Divider,{...r}),e.createElement(t.Item,{value:"none",exclusive:!0,exclusiveGroup:"symptoms-list"},"No, I do not have any of these symptoms"))};var s,n,m;o.parameters={...o.parameters,docs:{...(s=o.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    children: 'or'
+  },
+  render: args => <Checkbox {...Object.assign({}, CheckboxStories.WithExclusiveOption.args, CheckboxStories.default.args)}>
+      <Checkbox.Item value="sore throat" exclusiveGroup="symptoms-list">
+        Sore throat
+      </Checkbox.Item>
+      <Checkbox.Item value="runny nose" exclusiveGroup="symptoms-list">
+        Runny nose
+      </Checkbox.Item>
+      <Checkbox.Item value="muscle or joint pain" exclusiveGroup="symptoms-list">
+        Muscle or joint pain
+      </Checkbox.Item>
+      <Checkbox.Divider {...args} />
+      <Checkbox.Item value="none" exclusive exclusiveGroup="symptoms-list">
+        No, I do not have any of these symptoms
+      </Checkbox.Item>
+    </Checkbox>
+}`,...(m=(n=o.parameters)==null?void 0:n.docs)==null?void 0:m.source}}};const G=["Default"];export{o as Default,G as __namedExportsOrder,f as default};
