f5e4159533f1f5c6456a3c17a61e73de6beb9696
Rowell Heria <rowell+dev@heria.uk>
2024-06-03 19:25:07 +0100

    feat: add a-to-z and back to top

---
+++ packages/nhsuk-frontend-react/src/components/navigation/back-to-top-link/BackToTopLink.stories.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import type { Meta, StoryObj } from '@storybook/react';
+import { BackToTopLink } from './BackToTopLink';
+
+const meta: Meta<typeof BackToTopLink> = {
+  title: 'Components/Navigation/Back To Top Link',
+  component: BackToTopLink,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof BackToTopLink>;
+
+export const Default: Story = {
+  args: {
+    href: '#top',
+  },
+  render: (args) => <BackToTopLink {...args}>Back to top</BackToTopLink>,
+};
