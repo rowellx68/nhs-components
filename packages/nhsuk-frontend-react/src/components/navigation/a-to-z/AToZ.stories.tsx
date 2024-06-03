81e6b7ff551b1d934b0df222783345dcb22c90c4
Rowell Heria <rowell+dev@heria.uk>
2024-06-03 19:25:07 +0100

    feat: add a-to-z and back to top

---
+++ packages/nhsuk-frontend-react/src/components/navigation/a-to-z/AToZ.stories.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import type { Meta, StoryObj } from '@storybook/react';
+import { AToZ } from './AToZ';
+
+const meta: Meta<typeof AToZ> = {
+  title: 'Components/Navigation/A to Z',
+  component: AToZ,
+  subcomponents: {
+    'AToZ.Item': AToZ.Item,
+  } as Record<string, React.ComponentType<any>>,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof AToZ>;
+
+export const Default: Story = {
+  render: (args) => (
+    <AToZ {...args} aria-label="A to Z Navigation">
+      <AToZ.Item href="#A">A</AToZ.Item>
+      <AToZ.Item href="#B" noItems>
+        B
+      </AToZ.Item>
+      <AToZ.Item href="#C">C</AToZ.Item>
+      <AToZ.Item href="#D">D</AToZ.Item>
+      <AToZ.Item href="#E">E</AToZ.Item>
+      <AToZ.Item href="#F">F</AToZ.Item>
+      <AToZ.Item href="#G">G</AToZ.Item>
+      <AToZ.Item href="#H">H</AToZ.Item>
+      <AToZ.Item href="#I">I</AToZ.Item>
+      <AToZ.Item href="#J">J</AToZ.Item>
+      <AToZ.Item href="#K">K</AToZ.Item>
+      <AToZ.Item href="#L">L</AToZ.Item>
+      <AToZ.Item href="#M">M</AToZ.Item>
+      <AToZ.Item href="#N">N</AToZ.Item>
+      <AToZ.Item href="#O">O</AToZ.Item>
+      <AToZ.Item href="#P">P</AToZ.Item>
+      <AToZ.Item href="#Q">Q</AToZ.Item>
+      <AToZ.Item href="#R">R</AToZ.Item>
+      <AToZ.Item href="#S">S</AToZ.Item>
+      <AToZ.Item href="#T">T</AToZ.Item>
+      <AToZ.Item href="#U">U</AToZ.Item>
+      <AToZ.Item href="#V">V</AToZ.Item>
+      <AToZ.Item href="#W">W</AToZ.Item>
+      <AToZ.Item href="#X">X</AToZ.Item>
+      <AToZ.Item href="#Y">Y</AToZ.Item>
+      <AToZ.Item href="#Z" noItems>
+        Z
+      </AToZ.Item>
+    </AToZ>
+  ),
+};
---
+++ packages/nhsuk-frontend-react/src/components/navigation/a-to-z/AToZ.tsx
@@ -0,0 +1,85 @@
+import React from 'react';
+import { ElementProps } from '@/types/shared';
+import clsx from 'clsx';
+import { Factory, factory } from '@/internal/factory/factory';
+import { PolymorphicFactory } from '@/internal/factory/polymorphic-factory';
+import { Base } from '@/components/core/base/Base';
+
+export type AToZProps = { 'aria-label': string } & ElementProps<
+  'nav',
+  'aria-label'
+>;
+
+type AToZFactory = Factory<{
+  props: AToZProps;
+  ref: HTMLDivElement;
+  staticComponents: {
+    Item: typeof AToZItem;
+  };
+}>;
+
+const AToZ = factory<AToZFactory>(
+  (
+    {
+      children,
+      className,
+      role = 'navigation',
+      'aria-label': ariaLabel = 'A to Z Navigation',
+      ...props
+    }: AToZProps,
+    ref,
+  ) => {
+    return (
+      <nav
+        className={clsx(
+          'nhsuk-u-margin-bottom-4 nhsuk-u-margin-top-4',
+          className,
+        )}
+        role={role}
+        aria-label={ariaLabel}
+        {...props}
+        ref={ref}
+      >
+        <ol className="nhsuk-list nhsuk-u-clear nhsuk-u-margin-0" role="list">
+          {children}
+        </ol>
+      </nav>
+    );
+  },
+);
+
+export type AToZItemProps = { noItems?: boolean } & ElementProps<'a'>;
+
+type AToZItemFactory = PolymorphicFactory<{
+  props: AToZItemProps;
+  defaultComponent: 'a';
+  defaultRef: HTMLAnchorElement;
+}>;
+
+const AToZItem = factory<AToZItemFactory>(
+  ({ children, className, noItems, ...props }: AToZItemProps, ref) => {
+    return (
+      <li className="nhsuk-u-margin-bottom-0 nhsuk-u-float-left nhsuk-u-margin-right-1 nhsuk-a-to-z-min-width">
+        <Base<any>
+          as={noItems ? 'span' : 'a'}
+          className={clsx(
+            'nhsuk-u-font-size-22 nhsuk-u-padding-2 nhsuk-u-display-block',
+            { 'nhsuk-u-secondary-text-color': noItems },
+            className,
+          )}
+          {...props}
+          ref={ref}
+        >
+          {children}
+        </Base>
+      </li>
+    );
+  },
+);
+
+AToZ.displayName = 'AToZ';
+AToZItem.displayName = 'AToZ.Item';
+
+AToZ.Item = AToZItem;
+
+export { AToZ, AToZItem };
