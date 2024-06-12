64d546dd772cee6d3a8a980b20f10918eba0118a
Rowell Heria <rowell+dev@heria.uk>
2024-06-12 23:00:09 +0100

    fix(nhsuk-frontend-react-extension): add missing button styles

---
+++ packages/nhsuk-frontend-react-extension/purgecss.config.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-undef */
+/**
+ * @type {import('purgecss').UserDefinedOptions}
+ */
+module.exports = {
+  content: ['./dist/**/*.js'],
+  css: ['./dist/**/*.css'],
+  output: './dist/nhsuk-extension.css',
+};
