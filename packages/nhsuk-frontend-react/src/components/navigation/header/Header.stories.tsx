9fbb5677579d790643b70129637551cdeb9f9a61
Rowell Heria <rowell+dev@heria.uk>
2024-09-05 10:17:12 +0100

    fix(nhsuk-frontend-react): update useEffect to trigger when NavList children update
    This moves the invocation of the header script into NavList so it can react to the changes to the children. Useful for when the links are dynamically rendred.

--- packages/nhsuk-frontend-react/src/components/navigation/header/Header.stories.tsx
+++ packages/nhsuk-frontend-react/src/components/navigation/header/Header.stories.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import type { Meta, StoryObj } from '@storybook/react';
-import { Header } from './Header';
+import { Header, HeaderProps } from './Header';
 
 /**
  * Use the appropriate header at the top of every page to show users they are on an NHS service and help them get started in finding what they need.
@@ -156,4 +156,57 @@ export const WithOrgansation: Story = {
       </Header.Nav>
     </Header>
   ),
+};
+
+const SampleNav = (props: HeaderProps) => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsVisible(true);
+    }, 2000);
+  }, []);
+
+  return (
+    <Header {...props}>
+      <Header.Container>
+        <Header.Logo href="#" aria-label="NHS homepage" />
+        <Header.Content>
+          <Header.Search
+            inputProps={{ visuallyHiddenText: 'Search the NHS website' }}
+            buttonProps={{ visuallyHiddenText: 'Search' }}
+          />
+        </Header.Content>
+      </Header.Container>
+      <Header.Nav>
+        <Header.NavList>
+          {isVisible ? (
+            <>
+              <Header.NavItem href="#">Health A-Z</Header.NavItem>
+              <Header.NavItem href="#">Live Well</Header.NavItem>
+              <Header.NavItem href="#">Mental health</Header.NavItem>
+              <Header.NavItem href="#">Care and support</Header.NavItem>
+              <Header.NavItem href="#">Pregnancy</Header.NavItem>
+              <Header.NavItem href="#">NHS services</Header.NavItem>
+              <Header.NavItem href="#" variant="home-link">
+                Home
+              </Header.NavItem>
+              <Header.MobileMenu />
+            </>
+          ) : (
+            <Header.NavItem href="#">Health A-Z</Header.NavItem>
+          )}
+        </Header.NavList>
+      </Header.Nav>
+    </Header>
+  );
+};
+
+export const WithDelayedNav: Story = {
+  args: {
+    organisationName: 'Anytown Anyplace',
+    organisationSplit: 'Anywhere',
+    organisationDescriptor: 'NHS Foundation Trust',
+  },
+  render: (args) => <SampleNav {...args} />,
 };
