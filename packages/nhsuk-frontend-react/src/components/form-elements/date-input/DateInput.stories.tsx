a455f2f0d6b3d950b317b6bc5a1366a63d46df6d
Rowell Heria <rowell+dev@heria.uk>
2024-09-19 12:23:29 +0100

    feat(nhsuk-frontend-react): update date input to accept name prefix

--- packages/nhsuk-frontend-react/src/components/form-elements/date-input/DateInput.stories.tsx
+++ packages/nhsuk-frontend-react/src/components/form-elements/date-input/DateInput.stories.tsx
@@ -39,6 +39,7 @@ export const Default: Story = {
   args: {
     label: 'Date of birth',
     hint: 'For example, 31 3 1980',
+    namePrefix: 'date-of-birth',
     value: {
       day: '31',
       month: '3',
@@ -53,6 +54,7 @@ export const WithError: Story = {
     label: 'Date of birth',
     hint: 'For example, 31 3 1980',
     error: 'Please enter a valid date of birth',
+    namePrefix: 'date-of-birth',
     value: {
       day: '',
       month: '',
@@ -66,6 +68,7 @@ export const WithErrorObject: Story = {
   args: {
     label: 'Date of birth',
     hint: 'For example, 31 3 1980',
+    namePrefix: 'date-of-birth',
     error: {
       day: 'Please enter a valid day.',
       month: 'Please enter a valid month.',
