2d664e20cd4ebcc47ec21c5ea4047fa60bdb1621
Rowell Heria <rowell+dev@heria.uk>
2024-09-27 12:13:24 +0100

    fix(nhsuk-frontend-react): replace span with p in error message

--- packages/nhsuk-frontend-react/src/components/form-elements/error-message/ErrorMessage.tsx
+++ packages/nhsuk-frontend-react/src/components/form-elements/error-message/ErrorMessage.tsx
@@ -3,8 +3,7 @@ import clsx from 'clsx';
 import { VisuallyHidden } from '@/components/core/visually-hidden/VisuallyHidden';
 import { ElementProps, WithVisuallyHiddenTextProps } from '@/types/shared';
 
-export type ErrorMessageProps = ElementProps<'span'> &
-  WithVisuallyHiddenTextProps;
+export type ErrorMessageProps = ElementProps<'p'> & WithVisuallyHiddenTextProps;
 
 const ErrorMessage = ({
   className,
@@ -13,10 +12,10 @@ const ErrorMessage = ({
   ...rest
 }: ErrorMessageProps) => {
   return (
-    <span className={clsx('nhsuk-error-message', className)} {...rest}>
+    <p className={clsx('nhsuk-error-message', className)} {...rest}>
       <VisuallyHidden>{visuallyHiddenText}</VisuallyHidden>
       {children}
-    </span>
+    </p>
   );
 };
 
