b592be8cb0401ed63a7b160d4d94aeb81600ee7d
Rowell Heria <rowell+dev@heria.uk>
2024-09-27 13:46:11 +0100

    fix(nhsuk-frontend-react): support for firstCellIsHeader in table

--- packages/nhsuk-frontend-react/src/components/content-presentation/table/Table.spec.tsx
+++ packages/nhsuk-frontend-react/src/components/content-presentation/table/Table.spec.tsx
@@ -5,10 +5,15 @@ import { composeStory } from '@storybook/react';
 import meta, {
   TwoColumn as TwoColumnStory,
   ThreeColumn as ThreeColumnStory,
+  ThreeColumnWithFirstCellAsHeader as ThreeColumnWithFirstCellAsHeaderStory,
 } from './Table.stories';
 
 const TwoColumn = composeStory(TwoColumnStory, meta);
 const ThreeColumn = composeStory(ThreeColumnStory, meta);
+const ThreeColumnWithFirstCellAsHeader = composeStory(
+  ThreeColumnWithFirstCellAsHeaderStory,
+  meta,
+);
 
 it('should render a two column table', () => {
   const { container } = render(<TwoColumn />);
@@ -25,6 +30,16 @@ it('should render a three column table', () => {
   expect(container.querySelector('caption')).toHaveTextContent(
     'Ibuprofen tablet dosages for children',
   );
+  expect(
+    container.querySelector('table.nhsuk-table-responsive'),
+  ).toBeInTheDocument();
+  expect(container).toMatchSnapshot();
+});
+
+it('should render a three column table with the first cell as a header', () => {
+  const { container } = render(<ThreeColumnWithFirstCellAsHeader />);
+
+  expect(container.querySelector('tbody > tr > th')).toBeInTheDocument();
   expect(
     container.querySelector('table.nhsuk-table-responsive'),
   ).toBeInTheDocument();
