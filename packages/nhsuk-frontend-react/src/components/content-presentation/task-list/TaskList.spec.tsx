ae270d34163cf4b3acd2db88b556ef910bcfa7f6
Rowell Heria <rowell+dev@heria.uk>
2025-02-15 10:36:55 +0000

    test: add missing tasklist tests

---
+++ packages/nhsuk-frontend-react/src/components/content-presentation/task-list/TaskList.spec.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { it, expect } from 'vitest';
+import { render } from '@testing-library/react';
+import { composeStory } from '@storybook/react';
+import meta, {
+  Default as DefaultStory,
+  MultipleSections as MultipleSectionsStories,
+} from './TaskList.stories';
+
+const Default = composeStory(DefaultStory, meta);
+const MultipleSections = composeStory(MultipleSectionsStories, meta);
+
+it('should render the TaskList component with the incomplete tasks', () => {
+  const { container } = render(<Default />);
+  expect(container.querySelector('.nhsuk-tag--blue')).toBeInTheDocument();
+});
+
+it('should render the TaskList component with the completed tasks', () => {
+  const { container } = render(<MultipleSections />);
+  expect(container.querySelector('.nhsuk-task-list__hint')).toBeInTheDocument();
+  expect(container).toMatchSnapshot();
+});
