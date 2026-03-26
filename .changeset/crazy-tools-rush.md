3937767560b84860c3c1e82a8c68b40d07fe0293
Rowell Heria <rowell+dev@heria.uk>
2026-03-26 21:45:47 +0000

    chore(nhsuk-frontend-react-extension): remove package
    Removed this package since these components are now included in the new version of nhsuk-frontend-react

---
+++ .changeset/crazy-tools-rush.md
@@ -0,0 +1,7 @@
+---
+"nhsuk-frontend-react": patch
+---
+
+chore(nhsuk-frontend-react-extension): remove package
+
+Removed this package since these components are now included in the new version of nhsuk-frontend-react
--- packages/nhsuk-frontend-react-extension/purgecss.config.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable no-undef */
-/**
- * @type {import('purgecss').UserDefinedOptions}
- */
-module.exports = {
-  content: ['./dist/**/*.mjs'],
-  css: ['./dist/**/*.css'],
-  output: './dist/nhsuk-extension.css',
-};
